e495fd08a36a8006745e51f01f1c9dcd060c1de5
pinakinkansara <Blackbird@2327>
2021-07-25 22:21:18 +0530

    - added icon - fixed view - removed home fragment

--- app/src/main/java/com/pinakin/giffresh/MainActivity.kt
+++ app/src/main/java/com/pinakin/giffresh/MainActivity.kt
@@ -1,25 +1,105 @@
 package com.pinakin.giffresh
 
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.pinakin.giffresh.databinding.ActivityMainBinding
+import com.pinakin.giffresh.ui.favourite.FavouriteGifFragment
+import com.pinakin.giffresh.ui.trending.GifViewModel
+import com.pinakin.giffresh.ui.trending.TrendingGifFragment
+import com.pinakin.giffresh.utils.hideKeyboard
 import com.pinakin.giffresh.utils.viewBindings
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private val binding by viewBindings(ActivityMainBinding::bind)
 
+    private val gifViewModel: GifViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appbarConfig = AppBarConfiguration(navController.graph)
-        binding.toolBar.setupWithNavController(navController, appbarConfig)
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        binding.tipSearch.setEndIconOnClickListener {
+            binding.edtSearch.text?.clear()
+            gifViewModel.fetchGifs()
+        }
+
+        binding.edtSearch.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                v.hideKeyboard()
+                gifViewModel.fetchGifs(binding.edtSearch.text.toString())
+                return@setOnEditorActionListener true
+            }
+
+            return@setOnEditorActionListener false
+        }
+
+        setUpTabs()
+
     }
+
+    private fun setUpTabs() {
+
+        TabLayoutMediator(binding.tab, binding.viewPager) { tab, position ->
+            var tabConfig = if (position == 0) {
+                Pair("Trending", R.drawable.ic_baseline_trending_up_24)
+            } else {
+                Pair("Favourite", R.drawable.ic_baseline_favorite_24)
+            }
+            tab.text = tabConfig.first
+            tab.setIcon(tabConfig.second)
+        }.attach()
+
+        binding.tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.toolBar.visibility = if (tab?.position == 1) {
+                    View.GONE
+                } else {
+                    View.VISIBLE
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+    }
+
+    inner class ViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+        private val fragments = listOf(
+            TrendingGifFragment(),
+            FavouriteGifFragment()
+        )
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+
+    }
+
 }
--- app/src/main/java/com/pinakin/giffresh/ui/trending/TrendingGifFragment.kt
+++ app/src/main/java/com/pinakin/giffresh/ui/trending/TrendingGifFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -23,28 +24,13 @@ class TrendingGifFragment : Fragment(R.layout.fragment_trending_gif) {
 
     private val binding by viewBindings(FragmentTrendingGifBinding::bind)
 
-    private val gifViewModel: GifViewModel by viewModels()
+    private val gifViewModel: GifViewModel by activityViewModels()
+
     private lateinit var gifAdapter: GifPagedAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        binding.tipSearch.setEndIconOnClickListener {
-            binding.edtSearch.text?.clear()
-            gifViewModel.fetchGifs()
-        }
-
-        binding.edtSearch.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-
-                v.hideKeyboard()
-                gifViewModel.fetchGifs(binding.edtSearch.text.toString())
-                return@setOnEditorActionListener true
-            }
-
-            return@setOnEditorActionListener false
-        }
 
         binding.recTrendingGif.layoutManager = LinearLayoutManager(requireContext())
         binding.recTrendingGif.addItemDecoration(
